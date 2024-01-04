c6e88af914dac9789c0c9113d5e146aabcc8a86a
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-01-04 16:59:24 -0300

    chore: vitest prisma environment setup.

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,38 @@
+import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { execSync } from 'node:child_process';
+
+import { randomUUID } from 'node:crypto';
 import { Environment } from 'vitest';
+
+
+function generateDatabaseURL(schema: string): string {
+
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable');
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set('schema', schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: 'prisma',
   async setup() {
-    console.log('running setup');
+    const schema = randomUUID();
+    const url = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = url;
+
+    execSync('npx prisma migrate deploy');
 
     return {
-      teardown() {
-        console.log('running teardown');
+      async teardown() {
+        const prisma = new PrismaClient();
+        await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schema}" CASCADE`);
+        await prisma.$disconnect();
       },
     };
   },
