db2c185b05ef400e0a6c0ad912d4d2fe061aeb9b
Ademir Giraldelli <16231827+aeagiraldelli@users.noreply.github.com>
2024-01-04 10:45:58 -0300

    feat: profile controller.

--- src/http/controllers/profile.controller.ts
+++ src/http/controllers/profile.controller.ts
@@ -1,7 +1,16 @@
+import { makeUserProfileUseCase } from '@/use-case/factories';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify();
+  const userProfileUseCase = makeUserProfileUseCase();
+  const { user } = await userProfileUseCase.exec({
+    userId: request.user.sub
+  });
 
-  return reply.status(200).send();
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined
+    }
+  });
 }
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,11 @@
 import { FastifyInstance } from 'fastify';
 import { authenticate, profile, register } from './controllers';
+import { verifyJWT } from './hooks';
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register);
   app.post('/sessions', authenticate);
 
-  app.get('/me', profile);
+  // authenticated routes.
+  app.get('/me', { onRequest: [verifyJWT] }, profile);
 }
